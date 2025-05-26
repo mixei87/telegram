fed5b18f72120706556b0d085d3870689cb39077
p <prime@robot.com>
2025-05-26 13:30:25 +0700

    add routers

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from src.db.init_db import init_db
+from src.routers.user import router as user_router
+
+
+# from src.routers.chat import router as chat_router
+# from src.routers.message import router as message_router
 
 
 @asynccontextmanager
@@ -14,17 +19,19 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(title="Messenger API", lifespan=lifespan)
+
+# Подключение роутов
+app.include_router(user_router)
+
+
+# app.include_router(chat_router)
+# app.include_router(message_router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
 
 
 if __name__ == "__main__":
