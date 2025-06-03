98f6c836db5d8b25519dcff2c45724be38040f58
p <prime@robot.com>
2025-06-03 20:05:44 +0700

    change chat service and repo

--- src/repositories/chat.py
+++ src/repositories/chat.py
@@ -2,14 +2,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 from sqlalchemy import select
 
-from src.models import Chat, User, ChatType
+from src.models import Chat, ChatType
 
 
 class ChatRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def create(self, name: str, chat_type: ChatType) -> Chat:
+    async def create_chat(self, name: str, chat_type: ChatType) -> Chat:
         chat = Chat(name=name, type=chat_type)
         self.session.add(chat)
         await self.session.flush()
@@ -22,7 +22,3 @@ class ChatRepository:
         stmt = (select(Chat).where(Chat.id == chat_id).options(joinedload(Chat.members)))
         result = await self.session.execute(stmt)
         return result.unique().scalars().first()
-
-    async def add_member(self, chat: Chat, user: User) -> None:
-        chat.members.append(user)
-        await self.session.flush()
--- src/services/chat.py
+++ src/services/chat.py
@@ -1,27 +1,46 @@
 from src.repositories.chat import ChatRepository
-from src.models import Chat, ChatType
+from src.models import Chat, ChatType, User
+from src.repositories.chat_member import ChatMemberRepository
 from src.services.chat_member import ChatMemberService
 from src.services.group import GroupService
+from src.services.user import UserService
+from src.core.exceptions import LogicError, NotFoundError
 
 
 class ChatService:
-    def __init__(self, chat_repo: ChatRepository, chat_member_service: ChatMemberService,
+    def __init__(self, chat_repo: ChatRepository, chat_member_repo: ChatMemberRepository, user_service: UserService,
+                 chat_member_service: ChatMemberService,
                  group_service: GroupService):
         self.chat_repo = chat_repo
+        self.chat_member_repo = chat_member_repo
+        self.user_service = user_service
         self.chat_member_service = chat_member_service
         self.group_service = group_service
 
-    async def create_personal_chat(self, name: str) -> Chat:
-        chat = await self.chat_repo.create(name, ChatType.PERSONAL)
+    async def create_personal_chat(self, creator_id: int, friend_id: int) -> Chat:
+        if creator_id == friend_id:
+            raise LogicError("Нельзя создать персональный чат с самим собой")
+
+        creator: User = await self.user_service.get_exist_user(creator_id)
+        friend: User = await self.user_service.get_exist_user(friend_id)
+
+        chat = await self.chat_repo.create_chat(friend.name, ChatType.PERSONAL)
+        await self.chat_member_repo.add_member(chat, creator)
+        await self.chat_member_repo.add_member(chat, friend)
         return chat
 
     async def create_group_chat(self, name: str, creator_id: int) -> Chat:
-        chat = await self.chat_repo.create(name, ChatType.GROUP)
-        await self.group_service.create_group(chat, creator_id=creator_id)
-        return chat
+        group = await self.group_service.create_group(name, creator_id=creator_id)
+        return group.chat
 
     async def get_chat(self, chat_id: int) -> Chat | None:
         return await self.chat_repo.get_chat_by_id(chat_id)
+
+    async def get_exist_chat(self, chat_id: int) -> Chat:
+        chat = await self.get_chat(chat_id)
+        if chat is None:
+            raise NotFoundError(f"Чат с id: {chat_id} не найден")
+        return chat
 
     async def get_chat_with_members(self, chat_id: int) -> Chat | None:
         return await self.chat_repo.get_chat_by_id_with_members(chat_id)
