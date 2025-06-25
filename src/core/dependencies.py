84166ec2c16e6c11762c172f45d59560ee43513a
p <prime@robot.com>
2025-06-26 02:45:28 +0700

    add redis

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -1,18 +1,21 @@
 from typing import Annotated
+
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.core.redis_config import redis
 from src.db.session import get_async_session
-from src.repositories.chat_member import ChatMemberRepository
-from src.repositories.user import UserRepository
 from src.repositories.chat import ChatRepository
+from src.repositories.chat_member import ChatMemberRepository
 from src.repositories.group import GroupRepository
-from src.services.user import UserService
+from src.repositories.message import MessageRepository
+from src.repositories.user import UserRepository
 from src.services.chat import ChatService
 from src.services.chat_member import ChatMemberService
 from src.services.group import GroupService
-from src.repositories.message import MessageRepository
 from src.services.message import MessageService
+from src.services.redis_chat import RedisChatService
+from src.services.user import UserService
 
 SesionDepends = Annotated[AsyncSession, Depends(get_async_session)]
 
@@ -42,63 +45,54 @@ def get_user_service(user_repo: UserRepository = Depends(get_user_repo)) -> User
 
 
 def get_chat_member_service(
-        chat_member_repo: ChatMemberRepository = Depends(get_chat_member_repo),
-        user_service: UserService = Depends(get_user_service)
+    chat_member_repo: ChatMemberRepository = Depends(get_chat_member_repo),
+    user_service: UserService = Depends(get_user_service),
 ) -> ChatMemberService:
-    return ChatMemberService(
-        chat_member_repo,
-        user_service
-    )
+    return ChatMemberService(chat_member_repo, user_service)
 
 
 def get_group_service(
-        group_repo: GroupRepository = Depends(get_group_repo),
-        chat_repo: ChatRepository = Depends(get_chat_repo),
-        chat_member_repo: ChatMemberRepository = Depends(get_chat_member_repo),
-        chat_member_service: ChatMemberService = Depends(get_chat_member_service),
-        user_service: UserService = Depends(get_user_service)
+    group_repo: GroupRepository = Depends(get_group_repo),
+    chat_repo: ChatRepository = Depends(get_chat_repo),
+    chat_member_repo: ChatMemberRepository = Depends(get_chat_member_repo),
+    chat_member_service: ChatMemberService = Depends(get_chat_member_service),
+    user_service: UserService = Depends(get_user_service),
 ) -> GroupService:
     return GroupService(
-        group_repo,
-        chat_repo,
-        chat_member_repo,
-        chat_member_service,
-        user_service
+        group_repo, chat_repo, chat_member_repo, chat_member_service, user_service
     )
 
 
 def get_chat_service(
-        chat_repo: ChatRepository = Depends(get_chat_repo),
-        chat_member_repo: ChatMemberRepository = Depends(get_chat_member_repo),
-        user_service: UserService = Depends(get_user_service),
-        chat_member_service: ChatMemberService = Depends(get_chat_member_service),
-        group_service: GroupService = Depends(get_group_service)
+    chat_repo: ChatRepository = Depends(get_chat_repo),
+    chat_member_repo: ChatMemberRepository = Depends(get_chat_member_repo),
+    user_service: UserService = Depends(get_user_service),
+    chat_member_service: ChatMemberService = Depends(get_chat_member_service),
+    group_service: GroupService = Depends(get_group_service),
 ) -> ChatService:
     return ChatService(
-        chat_repo,
-        chat_member_repo,
-        user_service,
-        chat_member_service,
-        group_service
+        chat_repo, chat_member_repo, user_service, chat_member_service, group_service
     )
 
 
 def get_message_service(
-        message_repo: MessageRepository = Depends(get_message_repo),
-        chat_service: ChatService = Depends(get_chat_service),
-        user_service: UserService = Depends(get_user_service),
-        chat_member_service: ChatMemberService = Depends(get_chat_member_service)
+    message_repo: MessageRepository = Depends(get_message_repo),
+    chat_service: ChatService = Depends(get_chat_service),
+    user_service: UserService = Depends(get_user_service),
+    chat_member_service: ChatMemberService = Depends(get_chat_member_service),
 ) -> MessageService:
-    return MessageService(
-        message_repo,
-        chat_service,
-        user_service,
-        chat_member_service
-    )
+    return MessageService(message_repo, chat_service, user_service, chat_member_service)
+
+
+def get_redis_chat_service() -> RedisChatService:
+    return RedisChatService(redis)
 
 
 UserServiceDepends = Annotated[UserService, Depends(get_user_service)]
 ChatServiceDepends = Annotated[ChatService, Depends(get_chat_service)]
 GroupServiceDepends = Annotated[GroupService, Depends(get_group_service)]
-ChatMemberServiceDepends = Annotated[ChatMemberService, Depends(get_chat_member_service)]
+ChatMemberServiceDepends = Annotated[
+    ChatMemberService, Depends(get_chat_member_service)
+]
 MessageServiceDepends = Annotated[MessageService, Depends(get_message_service)]
+RedisChatServiceDepends = Annotated[RedisChatService, Depends(get_redis_chat_service)]
---
+++ src/services/redis_chat.py
@@ -0,0 +1,26 @@
+import json
+
+from redis.asyncio import Redis
+
+
+class RedisChatService:
+    def __init__(self, redis: Redis):
+        self.redis = redis
+
+    async def add_user_to_chat(self, chat_id: int, user_id: int):
+        await self.redis.sadd(f"chat:{chat_id}:members", user_id)
+        await self.redis.expire(f"chat:{chat_id}:members", 86400)
+
+    async def remove_user_from_chat(self, chat_id: int, user_id: int):
+        await self.redis.srem(f"chat:{chat_id}:members", user_id)
+
+    async def is_user_in_chat(self, chat_id: int, user_id: int) -> bool:
+        return bool(await self.redis.sismember(f"chat:{chat_id}:members", str(user_id)))
+
+    async def get_chat_members(self, chat_id: int) -> list[int]:
+        members = await self.redis.smembers(f"chat:{chat_id}:members")
+        return [int(id_str) for id_str in members if id_str.isdigit()]
+
+    async def add_to_queue(self, user_id: int, message: json):
+        await self.redis.rpush(f"queue:user:{user_id}", message)
+        await self.redis.expire(f"queue:user:{user_id}", 86400)
